e680bfb34074912471616364aa610daea340ec12
Carlos Feliciano-Barba <c.feliciano2009@gmail.com>
2023-12-14 15:31:34 -0400

    Add 2023 experience and updates

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,10 @@ const title = "Software Engineer"
 
 const profileBullets = [
   "Experience with RESTful APIs and Database design using TDD",
+  "Experience with video conferencing, websockets and data streaming",
   "Experience with Elasticsearch and OpenSearch data insertion and querying",
-  "Advanced experience with CSS, HTML, DOM manipulation and responsive Web Development",
   "Experience with caching implementations for API responses and data calculations using Redis",
+  "Advanced experience with CSS, HTML, DOM manipulation and responsive Web Development",
   "Devops experience using Continuous Integration and Delivery in platforms such as AWS and Heroku",
   "Experience with React fetching and data libraries such as Redux, React Context, and React Query",
   "Proficient programmer in Ruby, Javascript, Typescript, and SQL",
@@ -21,37 +22,45 @@ const profileBullets = [
 
 const experiences: ExperienceType[] = [
   {
-    date: "2021 - Present",
+    date: "2023 - Present",
+    company: "Campfire",
+    location: "Utah, US. Remote",
+    role: "Software Architect",
+    description: "Led an engineering team, orchestrating both frontend and backend efforts to elevate a React application and Rails backend. Spearheaded the integration of Daily.js and Rails websockets, transforming it into a dynamic video platform. Refactored the frontend to reduce data calls, improve caching and reduce rendering cycles. Rearchitected the scheduling system, streamlining the process to prevent loss of event invitations."
+  },
+  {
+    date: "2021 - 2023",
     company: "Web3 Contracting",
     location: "Remote",
     role: "Software Architect",
-    description: "Led full-stack development of Web3 technologies for exchanges and NFT marketplaces. Deployed several SPA in React and Typescript with multi-chain MetaMask integration. Developed an NFT indexer that parsed blockchain events, read from IPFS and stored data in Elasticsearch to enable attribute filtering and ranking per address. Developed a proxy server in Express.js to respond with metadata information before loading a SPA for SEO readers such as Google, Twitter and Discord."
+    description: "Led the full-stack development of Web3 technologies for exchanges and NFT marketplaces. Successfully deployed multiple Single Page Applications (SPA) in React and Typescript, featuring seamless integration with MetaMask across multiple chains. Pioneered the development of an NFT indexer, leveraging blockchain events, IPFS, and Elasticsearch for advanced attribute filtering and ranking. Engineered a proxy server in Express.js to optimize SEO by providing metadata information prior to loading SPAs on platforms such as Google, Twitter, and Discord."
   },
   {
     date: "2017 - 2021",
-    company: "Waggl",
+    company: "Perceptyx (formerly Waggl)",
     location: "Sausalito, California. Remote",
     role: "Tech Lead",
-    description: "Led, trained and mentored multiple distributed development teams through planning, release of features, and maintenance updates. Developed test flows to improve the creation, maintenance and understanding of test suites in RSpec, Jest and Cypress. Managed, tested and deployed several Ruby on Rails upgrades from v3 to v5. Created a React.js development workflow to improve the development time and component creation using Material-UI and Styled Components. Adapted a legacy Backbone.js app to be able to render React.js without having to refactor the core app."
+    description: "Guided, trained, and mentored distributed development teams through planning, feature releases, and maintenance updates. Introduced effective testing flows using RSpec, Jest, and Cypress to enhance the creation and maintenance of test suites. Successfully managed the testing and deployment of Ruby on Rails upgrades, optimizing from v3 to v5. Implemented a streamlined React.js development workflow, utilizing Material-UI and Styled Components. Adapted a legacy Backbone.js app to seamlessly integrate React.js, enhancing development efficiency."
   },
   {
     date: "2015 - 2017",
     company: "CometaWorks",
     location: "San Juan, Puerto Rico. Remote",
     role: "Senior Software Engineer",
-    description: "Trained and led various teams in development, maintenance and deployment of several Ember.js WebApps and Ruby on Rails RESTful APIs. Developed mobile versions for desktop websites before the SEO cutoff date. Converted a Balanced Payments Implementation to use Stripe Connect and provided maintenance afterwards. Engineered a commissions software for an accounting department where CSV uploads would be used to input data into a PostgreSQL DB in order to run calculations and display them based on user permissions."
+    description: "Provided leadership in the development, maintenance, and deployment of Ember.js WebApps and Ruby on Rails RESTful APIs. Led teams through the creation of mobile versions for desktop websites and successfully converted a Balanced Payments Implementation to utilize Stripe Connect. Engineered a commissions software for an accounting department, optimizing CSV uploads for data input into a PostgreSQL DB, resulting in efficient calculations and user-permission-based displays."
   },
   {
     date: "2014",
     company: "Codelitt",
     location: "Miami, Florida. Remote",
     role: "FullStack Web Developer",
-    description: "Developed multiple agile websites for product validation as well as worked on payment platforms that distributed direct deposits(ACH) in USD and Bitcoin."
+    description: "Contributed to the development of agile websites for product validation and worked on payment platforms facilitating direct deposits (ACH) in both USD and Bitcoin. This platform was later acquired by ZenPayroll (now Gusto)."
   }
-]
+];
 
 const technicalBullets = [
   "Implemented PostgreSQL database replication using Makara and PgBouncer",
+  "Implemented video confecencing infrastructure using Daily.js, ActionCable and ReactQuery",
   "Adapted a legacy Backbone.js app to render React.js components with state management handled by Redux",
   "Migrated Heroku deployments into a Docker based Continuous Integration and Delivery flow using Github, TravisCI and AWS Elastic Beanstalk",
   "Engineered an atomic design component flow by discussing design patterns and mutations with the design teams"
